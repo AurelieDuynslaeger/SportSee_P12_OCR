2d5924332a34c83cef7127f386de7245bb0697ad
AureLeeLi <aduynslaeger@gmail.com>
2024-08-14 22:01:31 +0200

    ✅ main content grid : skeleton done for user dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,12 @@ function App() {
       <TopNav/>
       <LeftNav/>
       <div className='main_content'>
-
+        <div className='user_header'>User Header</div>
+        <div className="user_daily_activity">Daily Activity</div>
+        <div className="user_sessions_length">Sessions Lenght</div>
+        <div className="user_radar_graph">Radar Graph</div>
+        <div className="user_score">User Score</div>
+        <div className="user_nutrition">User Nutrition</div>
       </div>
     </>
   )
