3782c771127a99404c2400d92e10c2c6bffe0f32
AureLeeLi <aduynslaeger@gmail.com>
2024-08-14 22:46:47 +0200

    🎨 grdi structure : Daily Activity, User sessions length, user radar graph, user score & user nutrition + 🎨 UserNutritionCard component

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,34 @@ function App() {
       <TopNav/>
       <LeftNav/>
       <div className='main_content'>
-        <div className='user_header'>User Header</div>
-        <div className="user_daily_activity">Daily Activity</div>
+        <div className='user_header'>
+          <h1>Bonjour <span className='user_first_name'>Prénom</span></h1>
+          <p>Félicitations ! Vous avez explosé vos objectifs d&apos;hier !</p>
+        </div>
+        <div className="user_daily_activity">
+          <div className='daily_activity_header'>
+            <h2>Activité Quotidiennes</h2>
+            <div className='weight_kcal'>
+              <div className='weight'>
+                <img src="" alt="" />
+                <p>Poids (kg)</p>
+              </div>
+              <div className='kcal'>
+                <img src="" alt="" />
+                <p>Calories brûlées (kCal)</p>
+              </div>
+            </div>
+          </div>
+          <div className='daily_activity_barchart'>
+              Bar Chart
+          </div>
+        </div>
         <div className="user_sessions_length">Sessions Lenght</div>
         <div className="user_radar_graph">Radar Graph</div>
         <div className="user_score">User Score</div>
-        <div className="user_nutrition">User Nutrition</div>
+        <div className="user_nutrition">
+          {/* map User_main_data pour appeler 4 div => composant UserNutritionCard */}
+        </div>
       </div>
     </>
   )
---
+++ src/components/UserNutritionCard/UserNutritionCard.jsx
@@ -0,0 +1,24 @@
+// import IconSquare from "../IconSquare"
+
+//icons : fire/chicken/apple/burger
+
+// props : Icon, iconsquarebg
+
+const UserNutritionCard = () => {
+
+    //Fetch USER_MAIN_DATA nutrition
+
+  return (
+    <div className="user_nutrition_card">
+        {/* icon square */}
+        {/* <IconSquare icon={} bgcolor=""/> */}
+        {/* user_data */}
+        <div className="user_data">
+            <h3>User_main_data Item</h3>
+            <p>User_main_data Item name</p>
+        </div>
+    </div>
+  )
+}
+
+export default UserNutritionCard
